4e24c55cffe09920021d6a51cd5878a0c7d42520
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2023-09-19 15:01:14 +0200

    first commit

---
+++ js/picovina.js
@@ -0,0 +1,19 @@
+function dopravaslide1() {
+    document.getElementById('clanek1').style.display = "none";
+    document.getElementById('clanek2').style.display = "block";
+}
+
+function dolevaslide2() {
+    document.getElementById('clanek1').style.display = "block";
+    document.getElementById('clanek2').style.display = "none";
+}
+
+function dopravaslide2() {
+    document.getElementById('clanek3').style.display = "block";
+    document.getElementById('clanek2').style.display = "none";
+}
+
+function dolevaslide3() {
+    document.getElementById('clanek2').style.display = "block";
+    document.getElementById('clanek3').style.display = "none";
+}
